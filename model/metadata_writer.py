a34a6914350da17ef9ff7032f046b2f76562e1a8
FlossIt <117515644+FlossIt@users.noreply.github.com>
2026-06-18 03:27:35 +0200

    Revert "Final version"
    This reverts commit f02fe6573f6c27824f7cf7b52d7e210aa4c9c610.

--- model/metadata_writer.py
+++
@@ -1,6 +0,0 @@
-class MetadataWriter:
-    def __init__(self):
-        pass
-
-    def method(self, typ):
-        print(f"Zapisuji metadata typu: {typ}")
--- model/user_manager.py
+++
@@ -1,8 +0,0 @@
-class UserManager:
-    def __init__(self):
-        self.id_uzivatele = {}
-        self.log_uzivatelu = ""
-        self.historie_uzivatele = ""
-
-    def proved_tah(self):
-        return True
